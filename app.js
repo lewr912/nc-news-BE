4f1ac101a2bc1d72865353c97a82d72e3a9f8852
lewis912 <lewiscc93@gmail.com>
2025-10-22 12:52:27 +0100

    add /api/topics endpoint

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const express = require("express");
 const db = require("./db/connection");
 const app = express();
 
+app.use(express.json());
+
+app.get("/api/topics", (req, res) => {
+  return db.query(`SELECT * FROM topics`).then(({ rows }) => {
+    res.status(200).send({ topics: rows });
+  });
+});
+
+module.exports = app;
