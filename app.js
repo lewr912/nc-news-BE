23cc6d165ef5da5c3c1e51fe7ab2be39b4543bda
lewis912 <lewiscc93@gmail.com>
2025-10-23 12:28:04 +0100

    add model and controller for /api/articles/:article_id, refactor

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("./db/connection");
 const app = express();
 const { getTopics } = require("./controllers/topics.controllers");
-const { getArticles } = require("./controllers/articles.controllers");
+const { getArticles, getArticleById } = require("./controllers/articles.controllers");
 const { getUsers } = require("./controllers/users.controllers");
 
 app.use(express.json());
@@ -12,5 +12,7 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 
 app.get("/api/users", getUsers);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 module.exports = app;
--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,7 +1,17 @@
-const { fetchArticles } = require("../models/articles.models");
+const {
+  fetchArticles,
+  fetchArticleById,
+} = require("../models/articles.models");
 
 exports.getArticles = (request, response) => {
   return fetchArticles().then((rows) => {
     response.status(200).send({ articles: rows });
+  });
+};
+
+exports.getArticleById = (request, response) => {
+  const { article_id } = request.params;
+  return fetchArticleById(article_id).then((article) => {
+    response.status(200).send({ article: article });
   });
 };
