36683e186914554aa29af1ad6025f890ab58aa9a
lewis912 <lewiscc93@gmail.com>
2025-10-24 15:01:26 +0100

    add test for 400 error for patch request at /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -194,4 +194,15 @@ describe("PATCH /api/articles/:article_id", () => {
         expect(article).toHaveProperty("article_img_url", expect.any(String));
       });
   });
+  test("400: Responds with an error message when patch request is made with invalid data type", () => {
+    const updateVotes = { inc_votes: "hello" };
+    return request(app)
+    .patch("/api/articles/1")
+    .send(updateVotes)
+    .expect(400)
+    .then(({ body: { message}}) => {
+      expect(message).toBe("You have made a bad request")
+    })
+  })
+  
 });
