a2fa5342880cdc0e76be5ebfe20e6cc10c322005
lewis912 <lewiscc93@gmail.com>
2025-10-26 17:39:05 +0000

    add checkTopicExists model function

--- models/topics.models.js
+++ models/topics.models.js
@@ -5,3 +5,13 @@ exports.fetchTopics = () => {
     return rows;
   });
 };
+
+exports.checkTopicExists = (topic) => {
+  
+  return db.query(`SELECT * FROM  topics WHERE slug = $1`, [topic])
+  .then(({ rows, rowCount }) => {
+      if (rowCount === 0) {
+        return Promise.reject({ status: 404, message: "Not Found" });
+      }
+    });
+}
